b9ade93da89bae42cace63033cb696779030be5a
Manolo <manolovp94@gmail.com>
2024-09-11 13:36:53 -0600

    Title: AuthController integration test without AutoConfigureMockMvc.

--- src/test/java/com/yukode/loginapirestjwt/controller/AuthControllerIntregationTest.java
+++ src/test/java/com/yukode/loginapirestjwt/controller/AuthControllerIntregationTest.java
@@ -2,28 +2,17 @@ package com.yukode.loginapirestjwt.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.yukode.loginapirestjwt.dto.UserDTO;
 import com.yukode.loginapirestjwt.dto.UserLoginDTO;
-import com.yukode.loginapirestjwt.exception.UserAlreadyExistsException;
-import com.yukode.loginapirestjwt.exception.UserInvalidCredentialException;
-import com.yukode.loginapirestjwt.mapper.UserLoginMapper;
-import com.yukode.loginapirestjwt.mapper.UserMapper;
 import com.yukode.loginapirestjwt.model.UserModel;
 import com.yukode.loginapirestjwt.repository.UserRepository;
-import com.yukode.loginapirestjwt.service.UserService;
+import com.yukode.loginapirestjwt.util.PasswordEncoderUtil;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-
-import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
@@ -32,52 +21,24 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest // Loads the full application context
-//@WebAppConfiguration // Configures a WebApplicationContext
 @AutoConfigureMockMvc
 public class AuthControllerIntregationTest {
 
-    @Autowired
-    private WebApplicationContext webApplicationContext;
-
-    @MockBean
-    private UserService userService;
-
-    @MockBean
-    private UserMapper userMapper;
 
     @Autowired
     private MockMvc mockMvc;
 
     @Autowired
     private UserRepository userRepository;
+    
+    @Autowired
+    private PasswordEncoderUtil passwordEncoderUtil;
 
     @BeforeEach
     void setUp() {
+        
+        userRepository.deleteAll();
 
-        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
-
-//        userRepository.deleteAll();
-
-//        UserModel userModel = new UserModel();
-//        
-//        UserDTO userDTO = new UserDTO();
-//        
-//        userModel.setId_user(15L);
-//        userModel.setName("test");
-//        userModel.setLastname("testL");
-//        userModel.setEmail("test@gmail.com");
-//        userModel.setPhone("999999999");
-//        userModel.setPassword("myPasswordEncoded");
-//        userModel.setRole("ROLE_USER");
-//        
-//        userDTO.setName("test");
-//        userDTO.setLastname("testL");
-//        userDTO.setEmail("test@gmail.com");
-//        userDTO.setPhone("999999999");
-//        userDTO.setRole("ROLE_USER");
-//        
-//        when(userService.register(anyString(), anyString(), anyString(), anyString(), anyString(), anyString())).thenReturn(userModel);
-//        when(userMapper.toUserDto(any(UserModel.class))).thenReturn(userDTO);
     }
 
     @Test
@@ -89,6 +50,7 @@ public class AuthControllerIntregationTest {
                 .accept(MediaType.APPLICATION_JSON_VALUE)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .content(mapToJson(newUser)))
+                .andExpect(status().isOk())
                 .andReturn();
 
         assertNotNull(result);
@@ -99,21 +61,17 @@ public class AuthControllerIntregationTest {
     @Test
     void testRegisterUserAlreadyExists() throws Exception {
 
-//        UserModel userModel = new UserModel();
-//
-//        userModel.setName("test");
-//        userModel.setLastname("testL");
-//        userModel.setEmail("test@gmail.com");
-//        userModel.setPhone("999999999");
-//        userModel.setPassword("myPasswordEncoded");
-//        userModel.setRole("ROLE_USER");
-//        
-//        userRepository.save(userModel);
+        UserModel userModel = new UserModel();
+
+        userModel.setName("test");
+        userModel.setLastname("testL");
+        userModel.setEmail("test@gmail.com");
+        userModel.setPhone("999999999");
+        userModel.setPassword(passwordEncoderUtil.encoderPassword("123"));
+        userModel.setRole("ROLE_USER");
+        userRepository.save(userModel);
 
         UserLoginDTO userLoginDTO = builderUserDTO();
-
-        when(userService.register(anyString(), anyString(), anyString(), anyString(), anyString(), anyString()))
-                .thenThrow(new UserAlreadyExistsException("User already exists whit this email"));
 
         MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/api/auth/register")
                 .accept(MediaType.APPLICATION_JSON_VALUE)
@@ -132,13 +90,11 @@ public class AuthControllerIntregationTest {
 
         UserLoginDTO userLoginDTO = builderUserDTO();
 
-        when(userService.login(anyString(), anyString()))
-                .thenThrow(new UserInvalidCredentialException("Invalid user or email"));
-
         MvcResult result = (MvcResult) mockMvc.perform(MockMvcRequestBuilders.post("/api/auth/login")
                 .accept(MediaType.APPLICATION_JSON_VALUE)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .content(mapToJson(userLoginDTO))) // Convert to JSON and send JSON format
+                .andExpect(status().isUnauthorized())
                 .andReturn();
 
         assertNotNull(result);
@@ -161,7 +117,7 @@ public class AuthControllerIntregationTest {
         userLoginDTO.setLastname("testL");
         userLoginDTO.setEmail("test@gmail.com");
         userLoginDTO.setPhone("999999999");
-        userLoginDTO.setPassword("myPassword123");
+        userLoginDTO.setPassword("123");
         userLoginDTO.setRole("ROLE_USER");
 
         return userLoginDTO;
